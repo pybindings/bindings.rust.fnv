ee32569b79d1fc685367dc38f39cbf2d3e6164d7
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-02-26 19:36:04 +0100

    Fix Travis builds not working as intended

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
 # coding: utf-8
+from __future__ import absolute_import
+
 import os
 import sys
 
 build_dir = os.path.abspath(os.path.join(__file__, '..', 'build', 'lib'))
 sys.path.insert(0, build_dir)
+
+from . import test_fnv
