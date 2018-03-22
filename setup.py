62c39a1e13c91c822fc42b39cb7869496e89a8d1
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-03-22 20:21:56 +0100

    Use `setuptools-rust` automatic extension discovery

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ import setuptools_rust as rust
 setuptools.setup(
     setup_requires=[
         "setuptools",
-        "setuptools_rust",
+        "setuptools-rust",
     ],
-    rust_extensions=[
-        rust.RustExtension(
-            "bindings.rust.fnv",
-            "bindings/rust/Cargo.toml",
-            binding=rust.Binding.PyO3,
-            strip=rust.Strip.Debug,
-        )
-    ]
+    rust_extensions=rust.find_rust_extensions(
+        binding=rust.Binding.PyO3,
+        strip=rust.Strip.Debug,
+    )
 )
+
